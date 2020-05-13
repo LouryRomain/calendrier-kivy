9cb3fb7d0b8dd0049687eeb9b5586ca629d2fe31
LouryRomain <loury.romain@hotmail.fr>
2020-05-13 17:18:31 +0200

    'init'

---
+++ model.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import calendar
+import datetime
+
+
+class Model:
+    def __init__(self,list_date):
+        self.list_date=list_date
+
+
+
+if __name__ == "__main__":
+    model=Model()
